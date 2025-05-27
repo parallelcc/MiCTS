ec4cb20af4228c6899d1105501d1f5b6c141fe2c
Parallelc <realparallelc@gmail.com>
2025-05-27 21:09:13 +0800

    fix: unable to trigger when "Long press the home button" is set to "None" in Button shortcuts (#68)

--- app/src/main/java/com/parallelc/micts/hooker/LongPressHomeHooker.kt
+++ app/src/main/java/com/parallelc/micts/hooker/LongPressHomeHooker.kt
@@ -1,7 +1,7 @@
 package com.parallelc.micts.hooker
 
+import android.annotation.SuppressLint
 import android.content.Context
-import android.os.Bundle
 import com.parallelc.micts.config.XposedConfig.CONFIG_NAME
 import com.parallelc.micts.config.XposedConfig.DEFAULT_CONFIG
 import com.parallelc.micts.config.XposedConfig.KEY_HOME_TRIGGER
@@ -18,35 +18,58 @@ import java.lang.reflect.Field
 class LongPressHomeHooker {
     companion object {
         private lateinit var mContext: Field
+        private lateinit var mKeyCode: Field
 
+        @SuppressLint("PrivateApi")
         fun hook(param: SystemServerLoadedParam) {
-            val shortCutActionsUtils = param.classLoader.loadClass("com.miui.server.input.util.ShortCutActionsUtils")
-            module!!.hook(
-                shortCutActionsUtils.getDeclaredMethod("triggerFunction", String::class.java, String::class.java, Bundle::class.java, Boolean::class.java, String::class.java),
-                TriggerFunctionHooker::class.java
-            )
-            mContext = shortCutActionsUtils.getDeclaredField("mContext")
+            val miuiSingleKeyRule = param.classLoader.loadClass("com.android.server.policy.MiuiSingleKeyRule")
+            mContext = miuiSingleKeyRule.getDeclaredField("mContext")
             mContext.isAccessible = true
+            mKeyCode = miuiSingleKeyRule.getDeclaredField("mKeyCode")
+            mKeyCode.isAccessible = true
+            module!!.hook(
+                miuiSingleKeyRule.getDeclaredMethod("onLongPress", Long::class.java),
+                OnLongPressHooker::class.java
+            )
+            module!!.hook(
+                miuiSingleKeyRule.getDeclaredMethod("supportLongPress"),
+                SupportLongPressHooker::class.java
+            )
         }
 
         @XposedHooker
-        class TriggerFunctionHooker : Hooker {
+        class OnLongPressHooker : Hooker {
             companion object {
                 @JvmStatic
                 @BeforeInvocation
                 fun before(callback: BeforeHookCallback) {
-                    if (callback.args[1] == "long_press_home_key" || callback.args[1] == "long_press_home_key_no_ui") {
+                    if (mKeyCode.getInt(callback.thisObject) == 3) {
                         val prefs = module!!.getRemotePreferences(CONFIG_NAME)
                         if (prefs.getBoolean(KEY_HOME_TRIGGER, DEFAULT_CONFIG[KEY_HOME_TRIGGER] as Boolean)) {
                             val context = runCatching { mContext.get(callback.thisObject) as? Context }.getOrNull()
-                            callback.returnAndSkip(
-                                triggerCircleToSearch(
-                                    1,
-                                    context,
-                                    prefs.getBoolean(KEY_VIBRATE, DEFAULT_CONFIG[KEY_VIBRATE] as Boolean)
-                                )
+                            triggerCircleToSearch(
+                                1,
+                                context,
+                                prefs.getBoolean(KEY_VIBRATE, DEFAULT_CONFIG[KEY_VIBRATE] as Boolean)
                             )
+                            callback.returnAndSkip(null)
                         }
+                    }
+                }
+            }
+        }
+    }
+
+    @XposedHooker
+    class SupportLongPressHooker : Hooker {
+        companion object {
+            @JvmStatic
+            @BeforeInvocation
+            fun before(callback: BeforeHookCallback) {
+                if (mKeyCode.getInt(callback.thisObject) == 3) {
+                    val prefs = module!!.getRemotePreferences(CONFIG_NAME)
+                    if (prefs.getBoolean(KEY_HOME_TRIGGER, DEFAULT_CONFIG[KEY_HOME_TRIGGER] as Boolean)) {
+                        callback.returnAndSkip(true)
                     }
                 }
             }
